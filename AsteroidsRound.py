d1ebff74a06d4c78d2ee5b88816f82a27708e0f0
“mixturesolutions” <tuo16213@ttemple.edu>
2024-04-03 21:15:25 -0400

    updated config

--- AsteroidsRound.py
+++ AsteroidsRound.py
@@ -1,9 +1,10 @@
 import pygame
 from sprites import *
 from ship import *
+from config import *
 from asteroid import *
 import sys
-import config
+
 
 class Game:
     asteroid_timer = 0
@@ -22,6 +23,16 @@ class Game:
         self.ships = pygame.sprite.Group()
         self.asteroids = pygame.sprite.Group()
         self.player_bullets = pygame.sprite.Group()
+
+        # all variables for the ship class
+        self.ship_exist = False
+        self.game_timer = 0
+        self.spawn_timer_ship = 0
+        self.spawn_timer_reg_bullet = 0
+        self.spawn_timer_sp_bullet = 0
+        self.spawn_delay_ship = 30
+        self.spawn_delay_reg_bullet = 30
+        self.spawn_delay_sp_bullet = 60
 
     def new(self):
         
@@ -46,10 +57,10 @@ class Game:
         #game loop updates
         self.all_sprites.update()
 
-        config.SPAWN_TIMER_SHIP += 1
-        config.SPAWN_TIMER_SP_BULLET += 1
-        config.SPAWN_TIMER_REG_BULLET += 1
-        config.GAME_TIMER += 1
+        self.spawn_timer_ship += 1
+        self.spawn_timer_reg_bullet += 1
+        self.spawn_timer_sp_bullet += 1
+        self.game_timer += 1
         self.asteroid_timer += 1
         
         self.asteroid_alg()
@@ -63,31 +74,31 @@ class Game:
             ship.move()
 
         # create the ship based on time interval
-        if config.SPAWN_TIMER_SHIP >= config.SPAWN_DELAY_SHIP * FPS:
-            config.SPAWN_TIMER_SHIP = 0
+        if self.spawn_timer_ship >= self.spawn_delay_ship * FPS:
+            self.spawn_timer_ship = 0
             self.spawn_ship()
-            config.SHIP_EXIST = True #if destroyed, changes to false
+            self.ship_exist = True #if destroyed, changes to false
         
         # start shooting for special bullet
-        if config.SHIP_EXIST and config.SPAWN_TIMER_SP_BULLET >= config.SPAWN_DELAY_SP_BULLET * FPS:
+        if self.ship_exist and self.spawn_timer_sp_bullet >= self.spawn_delay_sp_bullet * FPS:
             for ship in self.ships:
                 ship.shoot_sp_bullet()
-            config.SPAWN_TIMER_SP_BULLET = 0
+            self.spawn_timer_sp_bullet = 0
         
         # Start shooting for regular bullet
-        if config.SHIP_EXIST and config.SPAWN_TIMER_REG_BULLET >= config.SPAWN_DELAY_REG_BULLET * FPS:
+        if self.ship_exist and self.spawn_timer_reg_bullet >= self.spawn_delay_reg_bullet * FPS:
             for ship in self.ships:
                 ship.shoot_reg_bullet()
-            config.SPAWN_TIMER_REG_BULLET = 0
+            self.spawn_timer_reg_bullet = 0
 
         # increase difficulty - every one minute increase difficulty and both ship and bullet time of spawn decrease by 5
-        if config.GAME_TIMER >= 60 and config.SPAWN_DELAY_SP_BULLET > 30:
+        if self.game_timer >= 60 and self.spawn_delay_sp_bullet > 30:
             #add a screen display of difficult level currently - to do
-            if config.SPAWN_DELAY_SHIP > 25:
-                config.SPAWN_DELAY_SHIP -= 5
-                config.SPAWN_DELAY_REG_BULLET -= 5
-            config.SPAWN_DELAY_SP_BULLET -= 5
-            config.GAME_TIMER = 0
+            if self.spawn_delay_ship > 25:
+                self.spawn_delay_ship -= 5
+                self.spawn_delay_reg_bullet -= 5
+            self.spawn_delay_sp_bullet -= 5
+            self.game_timer = 0
         
     #create background screen for game
     def draw(self):
@@ -115,9 +126,9 @@ class Game:
     def asteroid_alg(self):
         size = random.choice([BIG_ASTEROID_SIZE, MED_ASTEROID_SIZE, SM_ASTEROID_SIZE])
 
-        if config.GAME_TIMER >= 30:
+        if self.game_timer >= 30:
             self.asteroid_spawn_delay = 4
-        if config.GAME_TIMER >= 60:
+        if self.game_timer >= 60:
             self.asteroid_spawn_delay = 3
 
         if self.asteroid_timer >= self.asteroid_spawn_delay * FPS:
